1f054df80537bf7f4ff61863c87b49a097323d98
Luis Collazo <luis.collazo@me.com>
2015-08-28 09:41:11 -0400

    modified routes and styled new profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
-  resources :users_do
-    resource :profile
+      devise_for :users, controllers: { registrations: 'users/registrations' }
+      resources :users do
+        resource :profile
+      end
+      resources :contacts
+      root 'pages#home'
+      get '/about' => 'pages#about'
+    end
   end
-
-  resources :contacts
-  root 'pages#home'
-  get '/about' => 'pages#about'
-end
